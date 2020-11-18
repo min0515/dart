a962695c5e948c5fb478d63de5d6449d2dd3c4fe
min0515 <minsee1234@naver.com>
2020-11-18 11:16:22 +0900

    홍길동이 dart-1.py 코드를 추가함. 에러 메세지를 출력할 수 있다. 회사 이름을 뽑아내는 코드 작성.

--- dart-1.py
+++ dart-1.py
@@ -5,7 +5,8 @@ AUTH_KEY = "6335ba5b35141c24db0d616d89ad536ca8c6a9d6"
 args = {
     'bgn_de':'20201001',
     'end_de':'20201101',
-    'corp_cls':'K'
+    'corp_cls':'K',
+    'sort':'date'
 }
 
 args_str = ''
@@ -16,4 +17,11 @@ for k,v in args.items():
 
 res = requests.get('https://opendart.fss.or.kr/api/list.json?crtfc_key=%s%s' %(AUTH_KEY,args_str))
 data = res.json()
-print(data)
+# print(data)
+
+if data['status'] != '000':
+    print(data(['message']))
+else:
+    data_list = data['list']
+    for d in data_list:
+        print(d['corp_name'])
